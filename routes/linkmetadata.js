c82a1aed459d09c1dc2b4acbe859c254ae6a0878
Somnath6646 <somutalks856@gmail.com>
2021-09-29 23:19:31 +0530

    meta type and extra added

--- routes/linkmetadata.js
+++ routes/linkmetadata.js
@@ -13,15 +13,44 @@ const metascraper = require('metascraper')([
   ])
   
   const got = require('got')
-  
+  const extract = require('meta-extractor');
 
 
 router.post("/metadata", async (req, res) => {
-        const link = req.body.link    
+        const link = req.body.link  
+        var type = ""
+        var extra = ""
+
+
         const { body: html, url } = await got(link)
+        
+        extract({ uri: link }, (err, res) =>{
+              if(res.ogSiteName.toLowerCase() == "medium" && res.ogType == 'article'){
+                type = "Medium"
+                extra = {
+                  "reading_time": res.twitterData1
+                }
+              }else if(res.ogSiteName.toLowerCase() == "youtube") {
+                type = "youtube.com"
+                if(res.pathname.toLowerCase().includes('playlist')){
+                  type = "YT Playlist"
+                }
+              }else if(res.ogSiteName.toLowerCase() == "twitter") {
+                type  ="Twitter"
+              }
+              else {
+                type =""    
+              }
+            });
+
+
         const metadata = await metascraper({ html, url })
+        metadata.type = type
+        metadata.extra = extra
         res.send(200, metadata)
-   
 });
+
+
+
 
 module.exports = router;
