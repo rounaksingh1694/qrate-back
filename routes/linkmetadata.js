628a0fb93c0dcf6235d0f10835bd3453f9db277e
Somnath6646 <somutalks856@gmail.com>
2021-09-30 11:14:16 +0530

    meta type and extra added 2

--- routes/linkmetadata.js
+++ routes/linkmetadata.js
@@ -1,56 +1,135 @@
+const axios = require("axios")
 const express = require("express");
 const router = express.Router();
+
 const metascraper = require('metascraper')([
     require('metascraper-author')(),
     require('metascraper-date')(),
-    require('metascraper-description')(),
-    require('metascraper-image')(),
-    require('metascraper-logo')(),
-    require('metascraper-clearbit')(),
-    require('metascraper-publisher')(),
-    require('metascraper-title')(),
-    require('metascraper-url')()
+    // require('metascraper-image')(),
   ])
   
   const got = require('got')
   const extract = require('meta-extractor');
 
+  
 
-router.post("/metadata", async (req, res) => {
+router.post("/metadata", async (req, resp) => {
         const link = req.body.link  
-        var type = ""
-        var extra = ""
-
+        var type;
+        var extra ;
+        var author = ""
 
         const { body: html, url } = await got(link)
-        
+        const metadata = await metascraper({ html, url })
+        author = metadata.author
         extract({ uri: link }, (err, res) =>{
-              if(res.ogSiteName.toLowerCase() == "medium" && res.ogType == 'article'){
+              console.log(res.ogSiteName.toLowerCase() + " " + res.ogType)
+              if(res.ogSiteName.toLowerCase() == 'medium' && res.ogType == 'article'){
+                
                 type = "Medium"
                 extra = {
                   "reading_time": res.twitterData1
                 }
+
+                sendResp(res, resp, metadata, type, extra, author)
+               
               }else if(res.ogSiteName.toLowerCase() == "youtube") {
+                
                 type = "youtube.com"
                 if(res.pathname.toLowerCase().includes('playlist')){
                   type = "YT Playlist"
                 }
+                var author_name;  
+                axios.get('https://www.youtube.com/oembed?url='+link)
+                .then(response => {
+                  author = response.data.author_name
+                  sendResp(res, resp, metadata, type, extra, author)
+                        })
+                      .catch(error => {
+                        console.log(error);
+                        });
+
+               
+
               }else if(res.ogSiteName.toLowerCase() == "twitter") {
+                
                 type  ="Twitter"
+                
+                axios.get('https://publish.twitter.com/oembed?url='+link)
+                .then(response => {
+                  console.log(response); 
+                  author = response.data.author_name
+                  sendResp(res, resp, metadata, type, extra, author)
+                        })
+                      .catch(error => {
+                        console.log(error);
+                        });
+
+               
+                
               }
               else {
+                console.log("aya hai bhai4")
                 type =""    
+                sendResp(res, resp, metadata, type, extra, author)
               }
+              
             });
+             
+         
 
-
-        const metadata = await metascraper({ html, url })
-        metadata.type = type
-        metadata.extra = extra
-        res.send(200, metadata)
+        
 });
 
+function sendResp(res, resp, metadata, type, extra, author){
+            var publisher = res.host
+            var description = res.ogDescription   
+            var title = res.title
+            var reso = {
+              "author": author ,
+              "date": metadata.date,
+              "image": res.ogImage,
+              "type": type,
+              "extra": extra,
+              "publisher": publisher,
+              "description": description,
+              "title": title
+            }
+            resp.status(200).send(reso)
+            
+            
+            console.log(type + " "+ extra);
+}
+
+/*
+{
+  "title": "Javascript course",
+  "author_name": "Hitesh Choudhary",
+  "author_url": "/c/HiteshChoudharydotcom",
+  "type": "video",
+  "height": 113,
+  "width": 200,
+  "version": "1.0",
+  "provider_name": "YouTube",
+  "provider_url": "https://www.youtube.com/",
+  "thumbnail_height": 360,
+  "thumbnail_width": 480,
+  "thumbnail_url": "https://i.ytimg.com/vi/2md4HQNRqJA/hqdefault.jpg",
+  "html": "<iframe width=\"200\" height=\"113\" src=\"https://www.youtube.com/embed/videoseries?list=PLRAV69dS1uWSxUIk5o3vQY2-_VKsOpXLD\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+}
+*/
 
 
+/*
+author:Hitesh Choudhary
+date: 
+descr: res.description
+img: thumbnail_url
+publisher: Youtube
+title: Javascript Course
+url: 
+type: Video
+extra: 
+*/
 
 module.exports = router;
